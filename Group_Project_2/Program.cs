0fc50aaf31d06f47355fabd0790a7ee928924d64
Kian Reynolds <S00237055@atu.ie>
2023-12-18 02:34:22 +0000

    Add project files.

---
+++ Group_Project_2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Group_Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Restaurant myRestaurant = new Restaurant("Best Foods Sligo", "O'Connell St", CuisineType.Italian);
+
+            
+            myRestaurant.AddMenuItem(new MenuItem { Name = "Spaghetti Bolognese", Description = "Classic Italian pasta dish", Price = 12.99m });
+            myRestaurant.AddMenuItem(new MenuItem { Name = "Margherita Pizza", Description = "Traditional pizza with tomato, mozzarella, and basil", Price = 14.99m });
+            myRestaurant.AddMenuItem(new MenuItem { Name = "Tiramisu", Description = "Italian dessert with coffee-soaked ladyfingers and mascarpone", Price = 8.99m });
+
+            
+            myRestaurant.DisplayMenu();
+
+            
+            Reservation reservation1 = new Reservation { NumberOfPeople = 4, Date = DateTime.Now.Date.AddDays(3), Time = "19:30" };
+            myRestaurant.MakeReservation(reservation1);
+
+            Console.WriteLine("Current Reservations:");
+            foreach (var reservation in myRestaurant.Reservations)
+            {
+                Console.WriteLine($"- {reservation.NumberOfPeople} people on {reservation.Date.ToShortDateString()} at {reservation.Time}");
+            }
+
+           
+            myRestaurant.CancelReservation(reservation1);
+
+           
+            Console.WriteLine("\nUpdated Reservations:");
+            foreach (var reservation in myRestaurant.Reservations)
+            {
+                Console.WriteLine($"- {reservation.NumberOfPeople} people on {reservation.Date.ToShortDateString()} at {reservation.Time}");
+            }
+        }
+    }
+}
+    
---
+++ Group_Project_2/Restaurant.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_2
+{
+    internal class Restaurant
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+        public List<MenuItem> MenuItems { get; set; }
+        public List<Reservation> Reservations { get; set; }
+
+        public Restaurant(string name, string location, CuisineType cuisine)
+        {
+            Name = name;
+            Location = location;
+            Cuisine = cuisine;
+            MenuItems = new List<MenuItem>();
+            Reservations = new List<Reservation>();
+        }
+
+        public void AddMenuItem(MenuItem menuItem)
+        {
+            MenuItems.Add(menuItem);
+        }
+
+        
+        public void RemoveMenuItem(MenuItem menuItem)
+        {
+            MenuItems.Remove(menuItem);
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine($"Menu for {Name} ({Cuisine} cuisine):");
+            foreach (var menuItem in MenuItems)
+            {
+                Console.WriteLine($"{menuItem.Name} - {menuItem.Description} - ${menuItem.Price}");
+            }
+        }
+
+        
+        public void MakeReservation(Reservation reservation)
+        {
+            Reservations.Add(reservation);
+            Console.WriteLine($"Reservation made for {reservation.NumberOfPeople} people on {reservation.Date} at {reservation.Time}.");
+        }
+
+        
+        public void CancelReservation(Reservation reservation)
+        {
+            Reservations.Remove(reservation);
+            Console.WriteLine($"Reservation canceled for {reservation.NumberOfPeople} people on {reservation.Date} at {reservation.Time}.");
+        }
+    }
+
+    
+    public enum CuisineType
+    {
+        Italian,
+        French,
+        Mexican,
+        Japanese,
+        American,
+        Other
+    }
+
+   
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    
+    public class Reservation
+    {
+        public int NumberOfPeople { get; set; }
+        public DateTime Date { get; set; }
+        public string Time { get; set; }
+    }
+
+
+}
